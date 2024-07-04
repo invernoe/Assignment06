11f22e12c00529d5b625d286ccf868fabbf2705d
invernoe <40373518+invernoe@users.noreply.github.com>
2024-07-04 22:33:49 +0200

    initial commit

---
+++ Assignment06/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Assignment06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
